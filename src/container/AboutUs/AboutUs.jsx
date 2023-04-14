bd48ac160ff28c4290eff2d6957e1aab3244f404
projectnum17 <egor.chornyy@gmail.com>
2023-04-14 14:27:39 +0300

    complete project

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { images } from '../../constants'
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding'>
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G letter" />
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about spoon" className='spoon__img' />
+        <p className="p__opensans">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, fugit. Consequatur repellendus rerum, odit necessitatibus sequi et dignissimos? Eaque quia odio aspernatur optio praesentium modi sunt placeat, natus dolor aliquam?</p>
+        <button type='button' className="custom__button">Know More</button>
+      </div>
+
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="Knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about spoon" className='spoon__img' />
+        <p className="p__opensans">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, fugit. Consequatur repellendus rerum, odit necessitatibus sequi et dignissimos? Eaque quia odio aspernatur optio praesentium modi sunt placeat, natus dolor aliquam?</p>
+        <button type='button' className="custom__button">Know More</button>
+      </div>
+
+    </div>
+    
+  </div>
+);
+
+export default AboutUs;
